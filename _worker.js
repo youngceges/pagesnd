19c1e08c6b6c4dd55e7d840526f052ff0c1d2ac1
youngceges <106800933+youngceges@users.noreply.github.com>
2022-06-03 08:21:59 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'vlhcdjsao.herokuapp.com'
+      url.hostname = 'vlcadakdf.herokuapp.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
